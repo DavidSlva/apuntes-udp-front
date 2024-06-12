5cbccefab54579b9e934322ed6544659851752fb
BenjaminArnello <benjamin.arnello_r@mail.udp.cl>
2024-06-12 19:36:43 -0400

    navbar top

--- src/layouts/components/Navbar.jsx
+++ src/layouts/components/Navbar.jsx
@@ -32,10 +32,24 @@ const Navbar = () => {
       onClick={handleClick}
       selectedKeys={[current]}
       mode="horizontal"
-      style={{ width: '100%' }}
+      className='w-full'
+      style={{ 
+ 
+        background: `linear-gradient(to right, #E54441, #E54441)`, // Red gradient background
+        color: 'white', // Text color of the Navbar
+      }}
     >
       {items.map(item => (
-        <Menu.Item key={item.key} icon={item.icon} style={{ flex: '1', textAlign: 'center' }}>
+        <Menu.Item 
+          key={item.key} 
+          icon={item.icon} 
+          style={{ 
+            flex: '1', 
+            textAlign: 'center',
+            background: current === item.key ? 'white' : `linear-gradient(to right, #E54441, #E54441)`, // Red gradient for non-selected items
+            color: current === item.key ? '#E54441' : 'white', // Red text color for non-selected items
+          }}
+        >
           {item.label}
         </Menu.Item>
       ))}
