b80b53f39d04a10db71904cb949e710ef63daf92
davidSlva <davidsilvaurrutia@gmail.com>
2024-07-12 04:08:13 -0400

    git ignore to production

--- src/config.js
+++ src/config.js
@@ -1,2 +1,6 @@
-export const API_URL = 'http://localhost:8000/api/v1';
-export const MEDIA_URL = 'http://localhost:8000';
+//Procces is not defined, how to solve this?
+
+export const API_URL =
+  process.env.REACT_APP_API_URL || 'http://localhost:8000/api/v1';
+export const MEDIA_URL =
+  process.env.REACT_APP_MEDIA_URL || 'http://localhost:8000';
